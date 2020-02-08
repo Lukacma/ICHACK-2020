f8a230c5360dbfa2c9765579820c4f2fc690577a
Limury <zioberni222@gmail.com>
2020-02-08 20:26:05 +0000

    removed maps

--- src/paper-dashboard-react-master/src/views/Dashboard.jsx
+++ src/paper-dashboard-react-master/src/views/Dashboard.jsx
@@ -42,83 +42,161 @@ class Dashboard extends React.Component {
       <>
         <div className="content">
           <Row>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-globe text-warning" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Capacity</p>
-                        <CardTitle tag="p">150GB</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update Now
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col lg="4" md="6" sm="6">
+                <Card className="card-user">
+                    <div className="image">
+                    <img
+                        alt="..."
+                        src={require("assets/img/damir-bosnjak.jpg")}
+                    />
+                    </div>
+                    <CardBody>
+                    <div className="author">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="avatar border-gray"
+                            src={require("assets/img/mike.jpg")}
+                        />
+                        <h5 className="title">Chet Faker</h5>
+                        </a>
+                        <p className="description">@chetfaker</p>
+                    </div>
+                    <p className="description text-center">
+                        "I like the way you work it <br />
+                        No diggity <br />I wanna bag it up"
+                    </p>
+                    </CardBody>
+                    <CardFooter>
+                    <hr />
+                    <div className="button-container">
+                        <Row>
+                        <Col className="ml-auto" lg="3" md="6" xs="6">
+                            <h5>
+                            12 <br />
+                            <small>Files</small>
+                            </h5>
+                        </Col>
+                        <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                            <h5>
+                            2GB <br />
+                            <small>Used</small>
+                            </h5>
+                        </Col>
+                        <Col className="mr-auto" lg="3">
+                            <h5>
+                            24,6$ <br />
+                            <small>Spent</small>
+                            </h5>
+                        </Col>
+                        </Row>
+                    </div>
+                    </CardFooter>
+                </Card>
+              
             </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-money-coins text-success" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Revenue</p>
-                        <CardTitle tag="p">$ 1,345</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-calendar" /> Last day
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col lg="4" md="6" sm="6">
+                <Card className="card-user">
+                    <div className="image">
+                    <img
+                        alt="..."
+                        src={require("assets/img/damir-bosnjak.jpg")}
+                    />
+                    </div>
+                    <CardBody>
+                    <div className="author">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="avatar border-gray"
+                            src={require("assets/img/mike.jpg")}
+                        />
+                        <h5 className="title">Chet Faker</h5>
+                        </a>
+                        <p className="description">@chetfaker</p>
+                    </div>
+                    <p className="description text-center">
+                         <span></span>
+                    </p>
+                    </CardBody>
+                    <CardFooter>
+                    <hr />
+                    <div className="button-container">
+                        <Row>
+                        <Col className="ml-auto" lg="3" md="6" xs="6">
+                            <h5>
+                            12 <br />
+                            <small>Files</small>
+                            </h5>
+                        </Col>
+                        <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                            <h5>
+                            2GB <br />
+                            <small>Used</small>
+                            </h5>
+                        </Col>
+                        <Col className="mr-auto" lg="3">
+                            <h5>
+                            24,6$ <br />
+                            <small>Spent</small>
+                            </h5>
+                        </Col>
+                        </Row>
+                    </div>
+                    </CardFooter>
+                </Card>
             </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-vector text-danger" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Errors</p>
-                        <CardTitle tag="p">23</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-clock" /> In the last hour
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col lg="4" md="6" sm="6">
+            <Card className="card-user">
+                    <div className="image">
+                    <img
+                        alt="..."
+                        src={require("assets/img/damir-bosnjak.jpg")}
+                    />
+                    </div>
+                    <CardBody>
+                    <div className="author">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="avatar border-gray"
+                            src={require("assets/img/mike.jpg")}
+                        />
+                        <h5 className="title">Chet Faker</h5>
+                        </a>
+                        <p className="description">@chetfaker</p>
+                    </div>
+                    <p className="description text-center">
+                        "I like the way you work it <br />
+                        No diggity <br />I wanna bag it up"
+                    </p>
+                    </CardBody>
+                    <CardFooter>
+                    <hr />
+                    <div className="button-container">
+                        <Row>
+                        <Col className="ml-auto" lg="3" md="6" xs="6">
+                            <h5>
+                            12 <br />
+                            <small>Files</small>
+                            </h5>
+                        </Col>
+                        <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                            <h5>
+                            2GB <br />
+                            <small>Used</small>
+                            </h5>
+                        </Col>
+                        <Col className="mr-auto" lg="3">
+                            <h5>
+                            24,6$ <br />
+                            <small>Spent</small>
+                            </h5>
+                        </Col>
+                        </Row>
+                    </div>
+                    </CardFooter>
+                </Card>
             </Col>
             <Col lg="3" md="6" sm="6">
               <Card className="card-stats">
@@ -147,84 +225,8 @@ class Dashboard extends React.Component {
               </Card>
             </Col>
           </Row>
-          <Row>
-            <Col md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Users Behavior</CardTitle>
-                  <p className="card-category">24 Hours performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboard24HoursPerformanceChart.data}
-                    options={dashboard24HoursPerformanceChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-history" /> Updated 3 minutes ago
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Email Statistics</CardTitle>
-                  <p className="card-category">Last Campaign Performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col md="8">
-              <Card className="card-chart">
-                <CardHeader>
-                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                  <p className="card-category">Line Chart with Points</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboardNASDAQChart.data}
-                    options={dashboardNASDAQChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="chart-legend">
-                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                    <i className="fa fa-circle text-warning" /> BMW 5 Series
-                  </div>
-                  <hr />
-                  <div className="card-stats">
-                    <i className="fa fa-check" /> Data information certified
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
+          
+          
         </div>
       </>
     );
